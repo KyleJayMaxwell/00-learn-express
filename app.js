27c6d312f8cfb32a560c66f2d5de6c2227cc0341
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-18 16:24:04 -0700

    The meaning of life, man

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+  app = express();
+
+var vegetables = [
+  'Carrots',
+  'Cucumber',
+  'Peas'
+];
+
+var port = 3333;
+// when a user visits the home page
+app.get("/", function (req, res) {
+  //Then send back the response: 'Hello World'
+  res.send('Hello World');
+});
+
+// When user goes to meaning-of-life they see "42"
+app.get("/meaning-of-life", function(req, res){
+  res.send("42");
+});
+
+// Start the server and make our web app available on localhost 3333
+app.listen(port, function (){
+  console.log("Starting a server on localhost:"+port)
+});
